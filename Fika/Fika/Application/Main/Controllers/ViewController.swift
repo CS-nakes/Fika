6e8e8af98b38c7e025bfb5772c88219e9f51fe3c
shadowezz <wenjie.cao@hotmail.com>
2021-06-15 00:13:04 +0800

    Add first profile page.

--- Fika/Fika/Application/Main/Controllers/ViewController.swift
+++ Fika/Fika/Application/Main/Controllers/ViewController.swift
@@ -12,6 +12,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+    }
+
+    @IBAction func onPress(_ sender: UIButton) {
+        performSegue(withIdentifier: "createProfile", sender: self)
     }
 
 }

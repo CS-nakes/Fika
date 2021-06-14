b0b13dd3d175409d9fcb85f9e9bf06f860f345e7
Hanming Zhu <hanming@huawen.com>
2021-06-14 11:10:46 +0800

    Restructure project and add scripts for bundle

---
+++ .github/scripts/unset_dynamic_bundle.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -o nounset
+. .github/scripts/config.sh
+
+sed "s/DEVELOPMENT_TEAM = $NEW_DEV_TEAM;/DEVELOPMENT_TEAM = $ORIGINAL_DEV_TEAM;/g" | sed "s/PRODUCT_BUNDLE_IDENTIFIER = $NEW_BUNDLE_ID;/PRODUCT_BUNDLE_IDENTIFIER = $ORIGINAL_BUNDLE_ID;/g"

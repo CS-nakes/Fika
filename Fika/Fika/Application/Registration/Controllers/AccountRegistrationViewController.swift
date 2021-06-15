4c11600fb1a069a3b52db45bc5bda2e971c003de
Hanming Zhu <hanming@huawen.com>
2021-06-15 19:40:37 +0800

    Clean up screens

--- Fika/Fika/Application/Registration/Controllers/AccountRegistrationViewController.swift
+++ Fika/Fika/Application/Registration/Controllers/AccountRegistrationViewController.swift
@@ -11,16 +11,6 @@ class AccountRegistrationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
-    @IBAction private func onBackButtonPressed(_ sender: UIButton) {
-        let trans = CATransition()
-        trans.type = CATransitionType.moveIn
-        trans.subtype = CATransitionSubtype.fromLeft
-        trans.duration = 0.35
-        self.navigationController?.view.layer.add(trans, forKey: nil)
-        navigationController?.popViewController(animated: false)
-    }
 }
--- Fika/Fika/Application/Registration/Controllers/CompanyCodeViewController.swift
+++ Fika/Fika/Application/Registration/Controllers/CompanyCodeViewController.swift
@@ -4,18 +4,7 @@ class CompanyCodeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        hideNavBar()
     }
 
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let _ = segue.destination as? AccountRegistrationViewController {
-            let trans = CATransition()
-            trans.type = CATransitionType.moveIn
-            trans.subtype = CATransitionSubtype.fromRight
-            trans.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
-            trans.duration = 0.35
-            self.navigationController?.view.layer.add(trans, forKey: nil)
-        }
-    }
 }

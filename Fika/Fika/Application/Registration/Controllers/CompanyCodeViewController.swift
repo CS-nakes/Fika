8717180894890bfa33eedd9c7385084353609be7
Hanming Zhu <hanming@huawen.com>
2021-06-15 20:41:59 +0800

    Update Registration to have modifiable title

--- Fika/Fika/Application/Registration/Controllers/CompanyCodeViewController.swift
+++ Fika/Fika/Application/Registration/Controllers/CompanyCodeViewController.swift
@@ -1,12 +1,17 @@
 import UIKit
 
 class CompanyCodeViewController: UIViewController {
+
     var hidesBackButton = false
+    var titleText = "Welcome to Fika"
+
+    @IBOutlet private var titleLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         hideNavBar()
         self.navigationItem.hidesBackButton = hidesBackButton
+        titleLabel.text = titleText
     }
 
 }

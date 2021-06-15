021fdb0b957007d84eeb3297140f47228fbd1f4b
chrystalquek <chrystalquek@yahoo.com.sg>
2021-06-15 09:47:05 +0800

    Initial commit for Landing Page

---
+++ Fika/Fika/Application/Landing/Controllers/LandingPageViewController.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+class LandingPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
+// MARK: - IBActions
+
+extension LandingPageViewController {
+
+    @IBAction private func onLoginPress(_ sender: UIButton) {
+
+    }
+
+}

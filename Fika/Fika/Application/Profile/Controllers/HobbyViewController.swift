f5870b2a4275b63323dcd4c170b01473bcc6cdf1
shadowezz <wenjie.cao@hotmail.com>
2021-06-15 15:46:10 +0800

    Add timeslot view.

---
+++ Fika/Fika/Application/Profile/Controllers/HobbyViewController.swift
@@ -0,0 +1,11 @@
+//
+//  File.swift
+//  Fika
+//
+//  Created by Cao Wenjie on 15/6/21.
+//
+import UIKit
+
+class HobbyViewController: UIViewController {
+
+}
---
+++ Fika/Fika/Application/Profile/Views/TimeSlot.swift
@@ -0,0 +1,11 @@
+//
+//  TimeSlot.swift
+//  Fika
+//
+//  Created by Cao Wenjie on 15/6/21.
+//
+import Foundation
+
+enum TimeSlot {
+    case breakfast, lunch, tea
+}

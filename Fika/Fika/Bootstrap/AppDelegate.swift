156c6ecbdc0eb6cc42008ed962de02820e6ce0d3
Clara Adora <52078276+claraadora@users.noreply.github.com>
2021-06-15 22:42:35 +0800

    Setup Firebase (#5)
    Co-authored-by: Hanming Zhu <hanming@huawen.com>

--- Fika/Fika/Bootstrap/AppDelegate.swift
+++ Fika/Fika/Bootstrap/AppDelegate.swift
@@ -6,16 +6,19 @@
 //
 
 import UIKit
-
+import Firebase
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Configure a `FirebaseApp` shared instance
+        FirebaseApp.configure()
+        
         return true
     }
-
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession,

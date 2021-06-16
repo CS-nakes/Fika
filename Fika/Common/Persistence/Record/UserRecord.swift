313e34020b6891f5f3dea7f414bbb6b48f425dc6
chrystalquek <chrystalquek@yahoo.com.sg>
2021-06-16 14:46:46 +0800

    Initial commit for firebase operations

---
+++ Fika/Common/Persistence/Record/UserRecord.swift
@@ -0,0 +1,17 @@
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+
+struct UserRecord {
+    @DocumentID private(set) var id: String?
+    private(set) var name: String
+    private(set) var position: String
+    // no companyId
+    private(set) var profilePicture: String
+    @ExplicitNull private(set) var introduction: String?
+    private(set) var preferredTimeslots: [Timeslot]
+
+}
+
+extension UserRecord: Codable {
+
+}
--- Fika/Models/User/User.swift
+++ Fika/Models/User/User.swift
@@ -1,10 +1,24 @@
 struct User {
     // email pw is in firebase
-    let id: String
+    let id: String?
     let name: String
     let position: String
     let companyId: String
-    let introduction: String
+    let introduction: String?
     let profilePicture: String // id to firebaseCloud storage
     let preferredTimeslots: [Timeslot]
+}
+
+extension User {
+    init(from record: UserRecord) throws {
+        self.init(
+            id: record.id,
+            name: record.name,
+            position: record.position,
+            companyId: "IGNORE",
+            introduction: record.introduction,
+            profilePicture: record.profilePicture,
+            preferredTimeslots: record.preferredTimeslots
+        )
+    }
 }

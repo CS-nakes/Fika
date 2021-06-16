4e5877d19ff64367e281f9c2ec2547e0bce90e14
Hanming Zhu <hanming@huawen.com>
2021-06-17 04:26:37 +0800

    Update temp token

--- Fika/Common/Constants/AgoraConstants.swift
+++ Fika/Common/Constants/AgoraConstants.swift
@@ -1,6 +1,6 @@
 enum AgoraConstants {
 
     static let appId = "29f941ada0484cd188769da6e7e4570e"
-    static let token = "00629f941ada0484cd188769da6e7e4570eIABP/cKa2vT6pLg/"
-        + "zW7ZAK+mVxFJCsTcyzYy/U/3O3Q6O59xbhcAAAAAEAARpytrwD/KYAEAAQC/P8pg"
+    static let token = "00629f941ada0484cd188769da6e7e4570eIADFgecTi70/XTY7my680e+BKGoH+KFb/"
+    + "K834QVtfLzs/J9xbhcAAAAAEAARpytruK/LYAEAAQC4r8tg"
 }
